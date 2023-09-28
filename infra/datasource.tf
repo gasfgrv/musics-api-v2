66473d6b7cfa1c72ac9c306480c122a9b98da8ad
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-09-28 00:08:24 -0300

    Infra descomentada

--- infra/datasource.tf
+++ infra/datasource.tf
@@ -1,9 +1,9 @@
-#data "aws_ami" "songs_ami" {
-#  most_recent = true
-#  owners      = ["099720109477"]
-#
-#  filter {
-#    name   = "name"
-#    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20230516"]
-#  }
-#}
+data "aws_ami" "musics_ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20230516"]
+  }
+}
