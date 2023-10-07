31d1ff94327d17c602f24555d072e6355a0282df
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-10-07 00:03:51 -0300

    Adição do WireMock para simular as chamadas à API do Spotify

---
+++ src/test/kotlin/com/github/gasfgrv/music_api/music/data/datasource/client/SpotifyHttpClientTest.kt
@@ -0,0 +1,10 @@
+package com.github.gasfgrv.music_api.music.data.datasource.client
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+
+class SpotifyHttpClientTest {
+  @Test
+  fun getTrackInfo() {
+  }
+}
